9c50ff852fb2b7149195ab60aa761be1073ba78a
unknown <richardmiao1645@gmail.com>
2023-01-24 16:09:43 -0800

    Add DayListItem component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+//Shows data for a single weekday
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "selected": props.selected,
+    "full": (props.spots === 0)
+  });
+
+  //Formats spots when there are 1 or 0 spots left
+  const formatSpots = function(numOfSpots) {
+    if (numOfSpots === 1) {
+      return `${numOfSpots} spot remaining`;
+    } else if (numOfSpots === 0) {
+      return "no spots remaining";
+    } else {
+      return `${numOfSpots} spots remaining`;
+    }
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}> 
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
